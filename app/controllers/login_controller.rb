7e8bbca6328f2a76e6fdf9f333e00d20b82597e8
Darryl Alan <drl.iceman@gmail.com>
2019-08-03 16:36:06 +0700

    Add login/logout functionality

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,4 +1,22 @@
 class LoginController < ApplicationController
   def index
   end
+
+  def login
+    user = User.find_by(username: params[:login][:username].downcase)
+    if user && user.authenticate(params[:login][:password])
+      session[:user_id] = user.id
+      flash[:success] = "Logged in successfully"
+      redirect_to root_path
+    else
+      flash.now[:error] = "Invalid login credentials"
+      render "index"
+    end
+  end
+
+  def logout
+    session[:user_id] = nil
+    flash[:success] = "You're logged out"
+    redirect_to login_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'chat#index'
   get 'login', to: 'login#index'
+  post 'login', to: 'login#login'
+  delete 'logout', to: 'login#logout'
 end
