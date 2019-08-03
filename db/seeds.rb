fafb56fca7eb23ef4ca9097c0e27781fe0d599de
Darryl Alan <drl.iceman@gmail.com>
2019-08-03 15:22:54 +0700

    Add Hirb, add user and message models and tables, display messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+a = User.create({ username: "a", password: "a" })
+b = User.create({ username: "b", password: "b" })
+c = User.create({ username: "c", password: "c" })
+d = User.create({ username: "d", password: "d" })
+e = User.create({ username: "e", password: "e" })
+
+Message.create({ body: "Hi my name is a", user: a })
+Message.create({ body: "Hi my name is b", user: b })
+Message.create({ body: "Hi my name is c", user: c })
+Message.create({ body: "Hi my name is d", user: d })
+Message.create({ body: "Hi my name is e", user: e })
